5cad3104b37815113f20b8314b8fe3cf1bcfce7c
AlexCampean19 <alex.campean24@gmail.com>
2022-08-11 17:09:05 +0300

    schimbari

--- scriptEndPointDetalii.js
+++ scriptEndPointDetalii.js
@@ -1,6 +1,7 @@
 async function randareHTMLDetaliu() {
     let produse = JSON.parse(sessionStorage.getItem('produse'));
     let categorySku = window.location.search ? window.location.search.replace('?sku=', '') : '';
+
     console.log(categorySku);
     if (categorySku) {
         let token = sessionStorage.getItem('token');
@@ -8,15 +9,16 @@ async function randareHTMLDetaliu() {
         await apiCall(url, token.replace(/"/g, ''), 'token', 'GET').then(result => {
             console.log(result.items);
             for (const [key, value] of Object.entries(JSON.parse(result).items)) {
-                let template = ' <div class="buc1"><img src="https://magento-demo.tk/media/catalog/product/' + value.media_gallery_entries[0].file + '"/></div><div class="buc2"><h1>' + value.name + '</h1> <div class="rating"><div class="stele"><p class="unu"><span>stea</span></p><p class="doi"><span>stea</span></p><p>(49)</p></div><div class="preturi2"><p>$' + value.price + '</p></div></div><p class="detaliu">' + value.detail + '</p><h4>Quantity</h4><div class="but2"><div class="butoane"><button type="button" class="minus"><span>minus</span></button><input class="numere-cantiate" type="number" name="quantity" value="1"><button type="button" class="plus"> <span>plus</span></button></div><a href="/" class="salemb"><span>Add to cart </span></a></div></div>';
+                let descriere = value.custom_attributes[12].value;
+                let template = ' <div class="buc1"><img src="https://magento-demo.tk/media/catalog/product/' + value.media_gallery_entries[0].file + '"/></div><div class="buc2"><h1>' + value.name + '</h1> <div class="rating"><div class="stele"><p class="unu"><span>stea</span></p><p class="doi"><span>stea</span></p><p>(49)</p></div><div class="preturi2"><p>$' + value.price + '</p></div></div><p class="detaliu">' + descriere + '</p><h4>Quantity</h4><div class="but2"><div class="butoane"><button type="button" class="minus"><span>minus</span></button><input class="numere-cantiate" type="number" name="quantity" value="1"><button type="button" class="plus"> <span>plus</span></button></div><a href="/" class="salemb"><span>Add to cart </span></a></div></div>';
                 document.querySelector(".bucati").innerHTML += template;
             }
         });
     } else {
         if (produse) {
             for (const [key, value] of Object.entries(produse.items)) {
-
-                let template = ' <div class="buc1"><img src="https://magento-demo.tk/media/catalog/product/' + value.media_gallery_entries[0].file + '"/></div><div class="buc2"><h1>' + value.name + '</h1> <div class="rating"><div class="stele"><p class="unu"><span>stea</span></p><p class="doi"><span>stea</span></p><p>(49)</p></div><div class="preturi2"><p>$' + value.price + '</p></div></div><p class="detaliu">' + value.detail + '</p><h4>Quantity</h4><div class="but2"><div class="butoane"><button type="button" class="minus"><span>minus</span></button><input class="numere-cantiate" type="number" name="quantity" value="1"><button type="button" class="plus"> <span>plus</span></button></div><a href="/" class="salemb"><span>Add to cart </span></a></div></div>';
+                let descriere = value.custom_attributes[12].value;
+                let template = ' <div class="buc1"><img src="https://magento-demo.tk/media/catalog/product/' + value.media_gallery_entries[0].file + '"/></div><div class="buc2"><h1>' + value.name + '</h1> <div class="rating"><div class="stele"><p class="unu"><span>stea</span></p><p class="doi"><span>stea</span></p><p>(49)</p></div><div class="preturi2"><p>$' + value.price + '</p></div></div><p class="detaliu">' + descriere + '</p><h4>Quantity</h4><div class="but2"><div class="butoane"><button type="button" class="minus"><span>minus</span></button><input class="numere-cantiate" type="number" name="quantity" value="1"><button type="button" class="plus"> <span>plus</span></button></div><a href="/" class="salemb"><span>Add to cart </span></a></div></div>';
                 document.querySelector(".bucati").innerHTML += template;
             }
         }
